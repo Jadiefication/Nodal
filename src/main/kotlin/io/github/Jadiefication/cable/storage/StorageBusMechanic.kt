dbc98a04c551f6286787ac74253865ab4c193adf
Jadiefication <jadeczjade008@gmail.com>
2025-07-26 14:15:49 +0200

    refactor: remove inventory

--- src/main/kotlin/io/github/Jadiefication/cable/storage/StorageBusMechanic.kt
+++ src/main/kotlin/io/github/Jadiefication/cable/storage/StorageBusMechanic.kt
@@ -1,23 +1,7 @@
 package io.github.Jadiefication.cable.storage
 
-import io.github.Jadiefication.cable.storage.gui.StorageBusGui
 import io.github.Jadiefication.redstoneflux.api.mechanics.EnergyTransporter
 import io.github.Jadiefication.redstoneflux.api.mechanics.InteractableMechanic
 import org.bukkit.event.player.PlayerInteractEvent
 
-class StorageBusMechanic : EnergyTransporter, InteractableMechanic {
-
-    override fun onRightClick(event: PlayerInteractEvent?) {
-        val block = event!!.clickedBlock!!
-        val player = event.player
-        player.openInventory(StorageBusGui(block).inventory)
-    }
-
-    override fun onLeftClick(event: PlayerInteractEvent?) {
-        return
-    }
-
-    override fun onPhysicalInteraction(event: PlayerInteractEvent?) {
-        return
-    }
-}
+class StorageBusMechanic : EnergyTransporter
